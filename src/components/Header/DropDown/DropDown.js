d749fede1bcad3ec5ad4ef09afd84a25c8803e60
raghukapur9 <64493087+raghukapur9@users.noreply.github.com>
2022-03-16 16:46:24 +0530

    enabled ethereum testnet switch

--- src/components/Header/DropDown/DropDown.js
+++ src/components/Header/DropDown/DropDown.js
@@ -30,7 +30,7 @@ function DropDown({ sortBy, chainChange }) {
       </button>
       {open && (
         <div className="absolute right-0 bg-dark-300 py-2 mt-2 bg-whitedivide-y divide-gray-600 rounded-md shadow-xl w-40">
-          {/* <p
+          <p
             className="option"
             onClick={() => {
               chainChange("Ethereum");
@@ -38,7 +38,7 @@ function DropDown({ sortBy, chainChange }) {
             }}
           >
             Ethereum
-          </p> */}
+          </p>
           <p
             className="option"
             onClick={() => {
