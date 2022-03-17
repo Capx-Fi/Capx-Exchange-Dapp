d82b330cf68c7148d14100c936fdc25bc59b424c
pulkit <pulkitm404@gmail.com>
2022-03-17 18:02:57 +0530

    conditional-rendering

--- src/pages/ProjectInfo/ProjectDescription.js
+++ src/pages/ProjectInfo/ProjectDescription.js
@@ -1,63 +1,67 @@
-import React from 'react';
-import './ProjectInfo.scss';
+import React from "react";
+import "./ProjectInfo.scss";
 
-import infoIcon from '../../assets/info.svg';
-import marketActivity from '../../assets/marketActivity.svg';
+import infoIcon from "../../assets/info.svg";
+import marketActivity from "../../assets/marketActivity.svg";
 
-import OuterContainer from './OuterContainer';
-import TokenListTable from './TokenListTable';
-import TokenActivityTable from './TokenActivityTable';
+import OuterContainer from "./OuterContainer";
+import TokenListTable from "./TokenListTable";
+import TokenActivityTable from "./TokenActivityTable";
 import MobileMarketActivity from "./MobileMarketActivity";
-import MobileBuyTable from '../../components/MobileTable/MobileTableBuy';
+import MobileBuyTable from "../../components/MobileTable/MobileTableBuy";
+import useWindowSize from "../../utils/windowSize";
 
 function ProjectDescription({ projectDetails, completeOrders, activeOrders }) {
+  const windowWidth = useWindowSize().width;
   return (
     <>
-    <div className="tablet:block phone:hidden">
-      <OuterContainer
-        title={'PROJECT DESCRIPTION'}
-        icon={infoIcon}
-        content={
-          projectDetails?.projectDescription ? (
-            <p className='p-4 max-h-32 w-full overflow-y-scroll'>
-              {projectDetails?.projectDescription}
-            </p>
-          ) : (
-            <div className='h-20 bg-loadingBG text-loadingBG bg-opacity-40 animate-pulse w-full'></div>
-          )
-        }
-      />
-      <OuterContainer
-        title={'MARKET ACTIVITY'}
-        icon={marketActivity}
-        completeOrders={completeOrders}
-        content={<TokenActivityTable completeOrders={completeOrders} />}
-      />
-      <OuterContainer
-        title={'TOKENS WITH $CAPX'}
-        icon={marketActivity}
-        activeOrders={activeOrders}
-        content={<TokenListTable activeOrders={activeOrders} />}
-      />
-      </div>
-
-      <div className="phone:block tablet:hidden">
-      <OuterContainer
-        title={'PROJECT DESCRIPTION'}
-        icon={infoIcon}
-        content={
-          projectDetails?.projectDescription ? (
-            <p className='p-4 max-h-32 w-full overflow-y-scroll'>
-              {projectDetails?.projectDescription}
-            </p>
-          ) : (
-            <div className='h-20 bg-loadingBG text-loadingBG bg-opacity-40 animate-pulse w-full'></div>
-          )
-        }
-      />
-        <MobileMarketActivity />
-        <TokenListTable activeOrders={activeOrders} />
-      </div>
+      {windowWidth > 768 ? (
+        <>
+          <OuterContainer
+            title={"PROJECT DESCRIPTION"}
+            icon={infoIcon}
+            content={
+              projectDetails?.projectDescription ? (
+                <p className="p-4 max-h-32 w-full overflow-y-scroll">
+                  {projectDetails?.projectDescription}
+                </p>
+              ) : (
+                <div className="h-20 bg-loadingBG text-loadingBG bg-opacity-40 animate-pulse w-full"></div>
+              )
+            }
+          />
+          <OuterContainer
+            title={"MARKET ACTIVITY"}
+            icon={marketActivity}
+            completeOrders={completeOrders}
+            content={<TokenActivityTable completeOrders={completeOrders} />}
+          />
+          <OuterContainer
+            title={"TOKENS WITH $CAPX"}
+            icon={marketActivity}
+            activeOrders={activeOrders}
+            content={<TokenListTable activeOrders={activeOrders} />}
+          />
+        </>
+      ) : (
+        <>
+          <OuterContainer
+            title={"PROJECT DESCRIPTION"}
+            icon={infoIcon}
+            content={
+              projectDetails?.projectDescription ? (
+                <p className="p-4 max-h-32 w-full overflow-y-scroll">
+                  {projectDetails?.projectDescription}
+                </p>
+              ) : (
+                <div className="h-20 bg-loadingBG text-loadingBG bg-opacity-40 animate-pulse w-full"></div>
+              )
+            }
+          />
+          <MobileMarketActivity />
+          <TokenListTable activeOrders={activeOrders} />
+        </>
+      )}
     </>
   );
 }
