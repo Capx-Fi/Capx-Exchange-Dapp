aeb65ab65c1e29625bab756da1f1cc6776184abe
pulkit <pulkitm404@gmail.com>
2022-03-23 17:42:03 +0530

    header-fixed--project-info-table-overflow-fixed

--- src/pages/ProjectInfo/ProjectDescription.js
+++ src/pages/ProjectInfo/ProjectDescription.js
@@ -16,53 +16,56 @@ function ProjectDescription({
   activeOrders,
   loading,
 }) {
+  const windowWidth = useWindowSize().width;
   return (
     <>
-      <div className="tablet:block phone:hidden">
-        <OuterContainer
-          title={"PROJECT DESCRIPTION"}
-          icon={infoIcon}
-          content={
-            projectDetails?.projectDescription ? (
-              <p className="p-4 max-h-32 w-full overflow-y-scroll">
-                {projectDetails?.projectDescription}
-              </p>
-            ) : (
-              <div className="h-20 bg-loadingBG text-loadingBG bg-opacity-40 animate-pulse w-full"></div>
-            )
-          }
-        />
-        <OuterContainer
-          title={"MARKET ACTIVITY"}
-          icon={marketActivity}
-          completeOrders={completeOrders}
-          content={<TokenActivityTable completeOrders={completeOrders} />}
-        />
-        <OuterContainer
-          title={"TOKENS WITH $CAPX"}
-          icon={marketActivity}
-          activeOrders={activeOrders}
-          content={<TokenListTable activeOrders={activeOrders} />}
-        />
-      </div>
-
-      <div className="phone:block tablet:hidden">
-        <OuterContainer
-          title={"PROJECT DESCRIPTION"}
-          icon={infoIcon}
-          content={
-            projectDetails?.projectDescription ? (
-              <p className="p-4 max-h-32 w-full overflow-y-scroll">
-                {projectDetails?.projectDescription}
-              </p>
-            ) : (
-              <div className="h-20 bg-loadingBG text-loadingBG bg-opacity-40 animate-pulse w-full"></div>
-            )
-          }
-        />
-        <MobileMarketActivity />
-        <TokenListTable activeOrders={activeOrders} loading={loading} />
-      </div>
+      {windowWidth > 768 ? (
+        <>
+          <OuterContainer
+            title={"PROJECT DESCRIPTION"}
+            icon={infoIcon}
+            content={
+              projectDetails?.projectDescription ? (
+                <p className="p-4 max-h-32 w-full overflow-y-scroll">
+                  {projectDetails?.projectDescription}
+                </p>
+              ) : (
+                <div className="h-20 bg-loadingBG text-loadingBG bg-opacity-40 animate-pulse w-full"></div>
+              )
+            }
+          />
+          <OuterContainer
+            title={"MARKET ACTIVITY"}
+            icon={marketActivity}
+            completeOrders={completeOrders}
+            content={<TokenActivityTable completeOrders={completeOrders} />}
+          />
+          <OuterContainer
+            title={"TOKENS WITH $CAPX"}
+            icon={marketActivity}
+            activeOrders={activeOrders}
+            content={<TokenListTable activeOrders={activeOrders} />}
+          />
+        </>
+      ) : (
+        <>
+          <OuterContainer
+            title={"PROJECT DESCRIPTION"}
+            icon={infoIcon}
+            content={
+              projectDetails?.projectDescription ? (
+                <p className="p-4 max-h-32 w-full overflow-y-scroll">
+                  {projectDetails?.projectDescription}
+                </p>
+              ) : (
+                <div className="h-20 bg-loadingBG text-loadingBG bg-opacity-40 animate-pulse w-full"></div>
+              )
+            }
+          />
+          <MobileMarketActivity />
+          <TokenListTable activeOrders={activeOrders} loading={loading} />
+        </>
+      )}
     </>
   );
 }
