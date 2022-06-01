dca9a6acf21bfacc9b4b1e60bc9df7b188a8ea2e
Sooraj M Singh <soorajsingh1999@gmail.com>
2022-06-01 14:09:46 +0530

    UX bug fixes and Improvements - Issue #29

--- src/containers/NothingHere/NothingHereInvestorDa.js
+++ src/containers/NothingHere/NothingHereInvestorDa.js
@@ -1,37 +1,39 @@
-import React from 'react';
-import NothingHereIcon from '../../assets/InvestorEmptyState.svg';
-import './NothingHere.scss';
-import NextIcon from '../../assets/next-black.svg';
+import React from "react";
+import NothingHereIcon from "../../assets/InvestorEmptyState.svg";
+import "./NothingHere.scss";
+import NextIcon from "../../assets/next-black.svg";
 
-import { useHistory } from 'react-router';
-import DeskCTA from '../../components/CTA/DeskCTA';
+import { useHistory } from "react-router";
+import DeskCTA from "../../components/CTA/DeskCTA";
+import { useLocation } from "react-router-dom";
 
 function NothingHereTrades() {
-  const history = useHistory();
-  return (
-    <article className='nothinghere'>
-      <section className='nothinghere_maincontainer'>
-        <div className='nothinghere_maincontainer_herocontainer'>
-        <figure>
-          <img
-            className='nothinghere_maincontainer_herocontainer_notfoundillustration'
-            src={NothingHereIcon}
-            alt='Nothing Here Illustration'
-          />
-          <figcaption className='nothinghere_maincontainer_herocontainer_subtitle'>
-            No Trades Found
-          </figcaption>
-        </figure>
-          {/* <DeskCTA
+	var location = useLocation();
+	const history = useHistory();
+	return (
+		<article className="nothinghere">
+			<section className="nothinghere_maincontainer">
+				<div className="nothinghere_maincontainer_herocontainer">
+					<figure>
+						<img
+							className="nothinghere_maincontainer_herocontainer_notfoundillustration"
+							src={NothingHereIcon}
+							alt="Nothing Here Illustration"
+						/>
+						<figcaption className="nothinghere_maincontainer_herocontainer_subtitle">
+							No Trades Found{" "}
+						</figcaption>
+					</figure>
+					{/* <DeskCTA
             onClick={() => history.push('/')}
             title='Add Tardes'
             classes='button'
             icon={NextIcon}
           /> */}
-        </div>
-      </section>
-    </article>
-  );
+				</div>
+			</section>
+		</article>
+	);
 }
 
 export default NothingHereTrades;
