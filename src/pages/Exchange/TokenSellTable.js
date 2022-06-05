e050c36ceac82a09674bcb6673041f2e0acd12fc
Sooraj M Singh <soorajsingh1999@gmail.com>
2022-06-05 14:05:47 +0530

    connection improvements and balance fetch bug fixes

--- src/pages/Exchange/TokenSellTable.js
+++ src/pages/Exchange/TokenSellTable.js
@@ -22,204 +22,222 @@ import BigNumber from "bignumber.js";
 import moment from "moment";
 import { useSelector } from "react-redux";
 import { getExchangeURL, getWrappedURL } from "../../constants/getChainConfig";
+import Web3 from "web3";
 const format = "HH:mm";
 BigNumber.config({
-  ROUNDING_MODE: 3,
-  DECIMAL_PLACES: 18,
-  EXPONENTIAL_AT: [-18, 36],
+	ROUNDING_MODE: 3,
+	DECIMAL_PLACES: 18,
+	EXPONENTIAL_AT: [-18, 36],
 });
 
 const { Column, ColumnGroup } = Table;
 
 function TokenSellTable({ filter, refresh }) {
-  const [tokenList, setTokenList] = useState(dummyDataExchange);
-  const [portfolioHoldings, setPortfolioHoldings] = useState([]);
-  const { active, account, chainId } = useWeb3React();
-  const ticker = useSelector((state) => state.exchange.sellTicker);
+	const [tokenList, setTokenList] = useState(dummyDataExchange);
+	const [portfolioHoldings, setPortfolioHoldings] = useState([]);
+	const { active, account, chainId, connector } = useWeb3React();
+	const ticker = useSelector((state) => state.exchange.sellTicker);
 
-  const exchangeURL = chainId && getExchangeURL(chainId);
-  const wrappedURL = chainId && getWrappedURL(chainId);
+	const exchangeURL = chainId && getExchangeURL(chainId);
+	const wrappedURL = chainId && getWrappedURL(chainId);
 
-  const [loading, setLoading] = useState(false);
-  let history = useHistory();
+	const [loading, setLoading] = useState(false);
+	const [web3, setWeb3] = useState(null);
 
-  useEffect(() => {
-    let nullSellTicker = ticker;
+	const setupProvider = async () => {
+		let result = await connector?.getProvider().then((res) => {
+			return res;
+		});
+		return result;
+	};
 
-    if (nullSellTicker) {
-      Object.keys(nullSellTicker).forEach((i) => (nullSellTicker[i] = ""));
-      dispatch(
-        setSellTicker({
-          ...nullSellTicker,
-          expiryDate: new Date(),
-          expiryTime: moment().utc().add(15, "minutes"),
-          price: 0,
-        })
-      );
-    }
-    fetchPortfolioHoldings();
-  }, [account, chainId, refresh]);
-  $(".ant-table-row").on("click", function () {
-    var selected = $(this).hasClass("highlight");
-    $(".ant-table-row").removeClass("highlight");
-    if (!selected) $(this).addClass("highlight");
-  });
-  const fetchPortfolioHoldings = async () => {
-    setLoading(true);
-    const holdings = await fetchPortfolio(account, wrappedURL);
-    const contractHoldings = await fetchContractBalances(
-      account,
-      exchangeURL,
-      wrappedURL,
-      chainId
-    );
-    const combinedHoldings = [...holdings, ...contractHoldings];
-    // check all the holdings and sum those who have same assetID
-    const portfolioHoldings = combinedHoldings.reduce((acc, curr) => {
-      const existing = acc.find((item) => item.assetID === curr.assetID);
-      if (!existing) {
-        return [...acc, curr];
-      } else {
-        existing.quantity = new BigNumber(curr.quantity)
-          .plus(existing.quantity)
-          .toString();
-        existing.balance = new BigNumber(curr.balance)
-          .plus(existing.balance)
-          .toString();
-        existing.maxQuantity = existing.quantity;
-        return acc;
-      }
-    }, []);
-    // convert quanityt to international currency system
-    const convertedPortfolioHoldings = portfolioHoldings.map((item) => {
-      const convertedItem = { ...item };
-      convertedItem.quantity = item.quantity.toString();
-      convertedItem.maxQuantity = convertedItem.quantity;
-      convertedItem.balance = item.balance.toString();
-      return convertedItem;
-    });
-    convertedPortfolioHoldings.sort((a, b) => {
-      return new Date(b.date) - new Date(a.date);
-    });
-    setPortfolioHoldings(convertedPortfolioHoldings);
-    setTokenList(convertedPortfolioHoldings);
-    setLoading(false);
-  };
+	useEffect(() => {
+		setupProvider().then((res) => {
+			setWeb3(new Web3(res));
+		});
+	}, [active, chainId]);
+	let history = useHistory();
 
-  useEffect(() => {
-    if (filter === "" || filter === undefined) {
-      setTokenList(portfolioHoldings);
-    } else {
-      const filteredList = portfolioHoldings.filter((token) => {
-        return token.asset.toLowerCase().includes(filter?.toLowerCase());
-      });
-      setTokenList(filteredList);
-    }
-  }, [filter]);
+	useEffect(() => {
+		let nullSellTicker = ticker;
 
-  function onChange(pagination, filters, sorter, extra) {
-    // console.log("params", pagination, filters, sorter, extra);
-  }
+		if (nullSellTicker) {
+			Object.keys(nullSellTicker).forEach((i) => (nullSellTicker[i] = ""));
+			dispatch(
+				setSellTicker({
+					...nullSellTicker,
+					expiryDate: new Date(),
+					expiryTime: moment().utc().add(15, "minutes"),
+					price: 0,
+				})
+			);
+		}
+		web3?.currentProvider && fetchPortfolioHoldings();
+	}, [account, chainId, refresh, web3]);
+	$(".ant-table-row").on("click", function () {
+		var selected = $(this).hasClass("highlight");
+		$(".ant-table-row").removeClass("highlight");
+		if (!selected) $(this).addClass("highlight");
+	});
+	const fetchPortfolioHoldings = async () => {
+		setLoading(true);
+		const holdings = await fetchPortfolio(account, wrappedURL);
+		const contractHoldings = await fetchContractBalances(
+			account,
+			exchangeURL,
+			wrappedURL,
+			chainId,
+			web3
+		);
+		const combinedHoldings = [...holdings, ...contractHoldings];
+		// check all the holdings and sum those who have same assetID
+		const portfolioHoldings = combinedHoldings.reduce((acc, curr) => {
+			const existing = acc.find((item) => item.assetID === curr.assetID);
+			if (!existing) {
+				return [...acc, curr];
+			} else {
+				existing.quantity = new BigNumber(curr.quantity)
+					.plus(existing.quantity)
+					.toString();
+				existing.balance = new BigNumber(curr.balance)
+					.plus(existing.balance)
+					.toString();
+				existing.maxQuantity = existing.quantity;
+				return acc;
+			}
+		}, []);
+		// convert quanityt to international currency system
+		const convertedPortfolioHoldings = portfolioHoldings.map((item) => {
+			const convertedItem = { ...item };
+			convertedItem.quantity = item.quantity.toString();
+			convertedItem.maxQuantity = convertedItem.quantity;
+			convertedItem.balance = item.balance.toString();
+			return convertedItem;
+		});
+		convertedPortfolioHoldings.sort((a, b) => {
+			return new Date(b.date) - new Date(a.date);
+		});
+		setPortfolioHoldings(convertedPortfolioHoldings);
+		setTokenList(convertedPortfolioHoldings);
+		setLoading(false);
+	};
 
-  const dispatch = useDispatch();
-  const navigateProject = async (address) => {
-    const projectAddress = await fetchProjectID(address, wrappedURL);
-    history.push(`/info/${projectAddress}`);
-  };
-  const windowWidth = useWindowSize().width;
-  return (
-    <div>
-    {windowWidth > 767 ? (
-      <div className="tokenListTableContainer">
-        <Table
-          dataSource={loading ? [] : tokenList}
-          locale={{
-            emptyText: loading ? "Loading Tokens..." : "No Token Found",
-          }}
-          pagination={false}
-          scroll={{ y: 430 }}
-          onChange={onChange}
-          onRow={(record) => {
-            return {
-              onClick: (e) => {
-                console.log("sell", record);
-                dispatch(setSellTicker(record));
-                dispatch(setTickerBalance(record.maxQuantity));
-              },
-            };
-          }}
-        >
-          <Column
-            title="Asset"
-            sorter={(a, b) => a.asset.localeCompare(b.asset)}
-            showSorterTooltip={false}
-            width={"30%"}
-            dataIndex="asset"
-            key="asset"
-            render={(value, row) => {
-              return (
-                <div onClick={() => navigateProject(row.assetID)}>
-                  <p className="text-white hover:text-primary-green-400 cursor-pointer upper:text-paragraph-2 tablet:text-caption-2">
-                    {value}
-                  </p>
-                </div>
-              );
-            }}
-          />
-          <Column
-            title="Quantity"
-            dataIndex="quantity"
-            key="quantity"
-            render={(value, row) => {
-              return (
-                <div className="upper:text-paragraph-2 desktop:text-caption-1 tablet:text-caption-2">
-                  {convertToInternationalCurrencySystem(value)}
-                </div>
-              );
-            }}
-          />
-          <Column
-            title="Unlock Date"
-            dataIndex="unlockDate"
-            key="unlockDate"
-            render={(value, row) => {
-              return (
-                <div className="upper:text-paragraph-2 desktop:text-caption-1 tablet:text-caption-2">
-                  {row.unlockDate}
-                </div>
-              );
-            }}
-          />
-          <Column
-            title=""
-            dataIndex="asset"
-            key="asset"
-            render={(value, row) => {
-              return (
-                <div className="border cursor-pointer border-grayLabel px-3 py-2 rounded-lg flex flex-row justify-center w-fit-content mx-auto upper:text-paragraph-2 tablet:text-caption-2">
-                  <img src={SellIcon} alt="deposit" className="mr-2" />
+	useEffect(() => {
+		if (filter === "" || filter === undefined) {
+			setTokenList(portfolioHoldings);
+		} else {
+			const filteredList = portfolioHoldings.filter((token) => {
+				return token.asset.toLowerCase().includes(filter?.toLowerCase());
+			});
+			setTokenList(filteredList);
+		}
+	}, [filter]);
 
-                  <p className="text-error-color-400 uppercase font-bold text-caption-2 upper:text-paragraph-2 tablet:text-caption-2">
-                    SELL
-                  </p>
-                </div>
-              );
-            }}
-          />
-        </Table>
-      </div> ) : (
-      <div>
-        <MobileTableSell
-          tokenList={tokenList}
-          loading={loading}
-          onChange={onChange}
-          setSellTicker={setSellTicker}
-          setBalance={setTickerBalance}
-          navigateProject={navigateProject}
-        />
-      </div> )}
-    </div>
-  );
+	function onChange(pagination, filters, sorter, extra) {
+		// console.log("params", pagination, filters, sorter, extra);
+	}
+
+	const dispatch = useDispatch();
+	const navigateProject = async (address) => {
+		const projectAddress = await fetchProjectID(address, wrappedURL);
+		history.push(`/info/${projectAddress}`);
+	};
+	const windowWidth = useWindowSize().width;
+	return (
+		<div>
+			{windowWidth > 767 ? (
+				<div className="tokenListTableContainer">
+					<Table
+						dataSource={loading ? [] : tokenList}
+						locale={{
+							emptyText: loading ? "Loading Tokens..." : "No Token Found",
+						}}
+						pagination={false}
+						scroll={{ y: 430 }}
+						onChange={onChange}
+						onRow={(record) => {
+							return {
+								onClick: (e) => {
+									console.log("sell", record);
+									dispatch(setSellTicker(record));
+									dispatch(setTickerBalance(record.maxQuantity));
+								},
+							};
+						}}
+					>
+						<Column
+							title="Asset"
+							sorter={(a, b) => a.asset.localeCompare(b.asset)}
+							showSorterTooltip={false}
+							width={"30%"}
+							dataIndex="asset"
+							key="asset"
+							render={(value, row) => {
+								return (
+									<div onClick={() => navigateProject(row.assetID)}>
+										<p className="text-white hover:text-primary-green-400 cursor-pointer upper:text-paragraph-2 tablet:text-caption-2">
+											{value}
+										</p>
+									</div>
+								);
+							}}
+						/>
+						<Column
+							title="Quantity"
+							dataIndex="quantity"
+							key="quantity"
+							render={(value, row) => {
+								return (
+									<div className="upper:text-paragraph-2 desktop:text-caption-1 tablet:text-caption-2">
+										{convertToInternationalCurrencySystem(value)}
+									</div>
+								);
+							}}
+						/>
+						<Column
+							title="Unlock Date"
+							dataIndex="unlockDate"
+							key="unlockDate"
+							render={(value, row) => {
+								return (
+									<div className="upper:text-paragraph-2 desktop:text-caption-1 tablet:text-caption-2">
+										{row.unlockDate}
+									</div>
+								);
+							}}
+						/>
+						<Column
+							title=""
+							dataIndex="asset"
+							key="asset"
+							render={(value, row) => {
+								return (
+									<div className="border cursor-pointer border-grayLabel px-3 py-2 rounded-lg flex flex-row justify-center w-fit-content mx-auto upper:text-paragraph-2 tablet:text-caption-2">
+										<img src={SellIcon} alt="deposit" className="mr-2" />
+
+										<p className="text-error-color-400 uppercase font-bold text-caption-2 upper:text-paragraph-2 tablet:text-caption-2">
+											SELL
+										</p>
+									</div>
+								);
+							}}
+						/>
+					</Table>
+				</div>
+			) : (
+				<div>
+					<MobileTableSell
+						tokenList={tokenList}
+						loading={loading}
+						onChange={onChange}
+						setSellTicker={setSellTicker}
+						setBalance={setTickerBalance}
+						navigateProject={navigateProject}
+					/>
+				</div>
+			)}
+		</div>
+	);
 }
 
 export default TokenSellTable;
--- src/utils/fetchContractBalances.js
+++ src/utils/fetchContractBalances.js
@@ -6,44 +6,46 @@ import { EXCHANGE_ABI } from "../contracts/ExchangeContract";
 
 import moment from "moment";
 import {
-  getExchangeContractAddress,
-  getUsdtContractAddress,
+	getExchangeContractAddress,
+	getUsdtContractAddress,
 } from "../constants/getChainConfig";
 const format = "HH:mm";
-const web3 = new Web3(Web3.givenProvider);
 BigNumber.config({
-  ROUNDING_MODE: 3,
-  DECIMAL_PLACES: 18,
-  EXPONENTIAL_AT: [-18, 36],
+	ROUNDING_MODE: 3,
+	DECIMAL_PLACES: 18,
+	EXPONENTIAL_AT: [-18, 36],
 });
 export const fetchContractBalances = async (
-  account,
-  exchangeURL,
-  wrappedURL,
-  chainId
+	account,
+	exchangeURL,
+	wrappedURL,
+	chainId,
+	web3
 ) => {
-  let fetchContractBalances = [];
-  let userContractHoldings = [];
-  const client = new ApolloClient({
-    uri: exchangeURL,
-    cache: new InMemoryCache(),
-  });
-  const exchangeContract = new web3.eth.Contract(
-    EXCHANGE_ABI,
-    chainId && getExchangeContractAddress(chainId)
-  );
+	let fetchContractBalances = [];
+	let userContractHoldings = [];
+	const client = new ApolloClient({
+		uri: exchangeURL,
+		cache: new InMemoryCache(),
+	});
+	const exchangeContract =
+		web3 &&
+		new web3.eth.Contract(
+			EXCHANGE_ABI,
+			chainId && getExchangeContractAddress(chainId)
+		);
 
-  // Getting stable coin
-  const stableCoin = chainId && getUsdtContractAddress(chainId);
-  console.log("-Stable coin-", stableCoin);
+	// Getting stable coin
+	const stableCoin = chainId && getUsdtContractAddress(chainId);
+	console.log("-Stable coin-", stableCoin);
 
-  // console.log("-chain ID-", chainId);
-  const liquidClient = new ApolloClient({
-    uri: wrappedURL,
-    cache: new InMemoryCache(),
-  });
+	// console.log("-chain ID-", chainId);
+	const liquidClient = new ApolloClient({
+		uri: wrappedURL,
+		cache: new InMemoryCache(),
+	});
 
-  const getAssets = `query{
+	const getAssets = `query{
         assets {
             id
             tokenTicker
@@ -51,7 +53,7 @@ export const fetchContractBalances = async (
         }
     }`;
 
-  const getHoldings = `query{
+	const getHoldings = `query{
         users (where: {id:"${account.toLowerCase()}"}){
             id
             lockedBalance (where: { assetID_not: "${stableCoin}",lockedValue_gt:0}){
@@ -65,125 +67,125 @@ export const fetchContractBalances = async (
         }
       }`;
 
-  const getAssetUnlockTime = `query{
+	const getAssetUnlockTime = `query{
         derivatives {
             id
             unlockTime
           }
       }`;
 
-  try {
-    const { data: assetData } = await client.query({
-      query: gql(getAssets),
-    });
-    const { data: holdingsData } = await client.query({
-      query: gql(getHoldings),
-    });
-    const { data: assetUnlockTime } = await liquidClient.query({
-      query: gql(getAssetUnlockTime),
-    });
+	try {
+		const { data: assetData } = await client.query({
+			query: gql(getAssets),
+		});
+		const { data: holdingsData } = await client.query({
+			query: gql(getHoldings),
+		});
+		const { data: assetUnlockTime } = await liquidClient.query({
+			query: gql(getAssetUnlockTime),
+		});
 
-    Object.entries(holdingsData.users).forEach((record) => {
-      let userData = record[1];
-      let userTotalData = userData.totalBalance;
-      let userLockedData = userData.lockedBalance;
-      let assetToLockedBalance = {};
-      Object.entries(userLockedData).forEach((locked) => {
-        assetToLockedBalance[locked[1].assetID] = locked[1].lockedValue;
-      });
-      Object.entries(userTotalData).forEach((total) => {
-        // Not printing stable coin again
-        // console.log(total[1].assetID, stableCoin);
-        if (
-          web3.utils.toChecksumAddress(total[1].assetID) ===
-          web3.utils.toChecksumAddress(stableCoin)
-        ) {
-          return;
-        }
+		Object.entries(holdingsData.users).forEach((record) => {
+			let userData = record[1];
+			let userTotalData = userData.totalBalance;
+			let userLockedData = userData.lockedBalance;
+			let assetToLockedBalance = {};
+			Object.entries(userLockedData).forEach((locked) => {
+				assetToLockedBalance[locked[1].assetID] = locked[1].lockedValue;
+			});
+			Object.entries(userTotalData).forEach((total) => {
+				// Not printing stable coin again
+				// console.log(total[1].assetID, stableCoin);
+				if (
+					web3.utils.toChecksumAddress(total[1].assetID) ===
+					web3.utils.toChecksumAddress(stableCoin)
+				) {
+					return;
+				}
 
-        let xData = {};
-        xData.asset = assetData.assets.find(
-          (asset) => asset.id === total[1].assetID
-        ).tokenTicker;
-        xData.decimal = assetData.assets.find(
-          (asset) => asset.id === total[1].assetID
-        ).tokenDecimal;
-        xData.assetID = total[1].assetID;
+				let xData = {};
+				xData.asset = assetData.assets.find(
+					(asset) => asset.id === total[1].assetID
+				).tokenTicker;
+				xData.decimal = assetData.assets.find(
+					(asset) => asset.id === total[1].assetID
+				).tokenDecimal;
+				xData.assetID = total[1].assetID;
 
-        // console.log(
-        //   assetUnlockTime.derivatives.find(
-        //     (derivative) => derivative.id === total[1].assetID
-        //   ),
-        //   "----"
-        // );
-        xData.unlockTime = assetUnlockTime.derivatives.find(
-          (derivative) => derivative.id === total[1].assetID
-        ).unlockTime;
+				// console.log(
+				//   assetUnlockTime.derivatives.find(
+				//     (derivative) => derivative.id === total[1].assetID
+				//   ),
+				//   "----"
+				// );
+				xData.unlockTime = assetUnlockTime.derivatives.find(
+					(derivative) => derivative.id === total[1].assetID
+				).unlockTime;
 
-        xData.totalBalance = new BigNumber(total[1].totalValue).toString();
-        xData.lockedBalance = assetToLockedBalance[total[1].assetID]
-          ? new BigNumber(assetToLockedBalance[total[1].assetID]).toString()
-          : 0;
-        // xData.actualBalance = new BigNumber(xData.totalBalance)
-        //   .minus(xData.lockedBalance)
-        //   .toString();
+				xData.totalBalance = new BigNumber(total[1].totalValue).toString();
+				xData.lockedBalance = assetToLockedBalance[total[1].assetID]
+					? new BigNumber(assetToLockedBalance[total[1].assetID]).toString()
+					: 0;
+				// xData.actualBalance = new BigNumber(xData.totalBalance)
+				//   .minus(xData.lockedBalance)
+				//   .toString();
 
-        xData.actualBalance = 0;
-        fetchContractBalances.push(xData);
-      });
-    });
-    userContractHoldings = await Promise.all(
-      fetchContractBalances.map(async (contractHolding) => {
-        // await console.log("Before issue");
-        // await console.log(
-        //   "Before issue values",
-        //   contractHolding.assetID,
-        //   account
-        // );
-        // await console.log(exchangeContract);
+				xData.actualBalance = 0;
+				fetchContractBalances.push(xData);
+			});
+		});
+		userContractHoldings = await Promise.all(
+			fetchContractBalances.map(async (contractHolding) => {
+				// await console.log("Before issue");
+				// await console.log(
+				//   "Before issue values",
+				//   contractHolding.assetID,
+				//   account
+				// );
+				// await console.log(exchangeContract);
 
-        let balance = await exchangeContract.methods
-          .unlockBalance(contractHolding.assetID, account)
-          .call();
-        // await console.log("After issue");
-        balance = new BigNumber(balance)
-          .dividedBy(Math.pow(10, contractHolding.decimal))
-          .toString();
-        contractHolding.actualBalance = balance;
-        const unixTime = contractHolding.unlockTime;
-        const date = new Date(unixTime * 1000);
-        let unlockDay = date.toLocaleDateString("en-US", {
-          day: "numeric",
-        });
-        let unlockMonth = date.toLocaleDateString("en-US", {
-          month: "long",
-        });
-        let unlockYear = date.toLocaleDateString("en-US", {
-          year: "numeric",
-        });
-        let displayDate = `${unlockDay} ${unlockMonth}, ${unlockYear}`;
-        return {
-          date,
-          asset: contractHolding.asset,
-          assetID: contractHolding.assetID,
-          price: null,
-          expiryDate: new Date(),
-          expiryTime: moment().utc().add(15, "minutes"),
-          unlockTime: contractHolding.unlockTime,
-          tokenDecimal: contractHolding.decimal,
-          quantity: contractHolding.actualBalance,
-          balance: contractHolding.actualBalance,
-          unlockDate: displayDate,
-          isContract: true,
-        };
-      })
-    );
-    userContractHoldings = userContractHoldings.filter(
-      (holding) => holding.quantity > 0
-    );
-  } catch (error) {
-    console.log(error);
-    // console.log("Holdings error");
-  }
-  return userContractHoldings;
+				let balance = await exchangeContract.methods
+					.unlockBalance(contractHolding.assetID, account)
+					.call();
+				// await console.log("After issue");
+				balance = new BigNumber(balance)
+					.dividedBy(Math.pow(10, contractHolding.decimal))
+					.toString();
+				contractHolding.actualBalance = balance;
+				const unixTime = contractHolding.unlockTime;
+				const date = new Date(unixTime * 1000);
+				let unlockDay = date.toLocaleDateString("en-US", {
+					day: "numeric",
+				});
+				let unlockMonth = date.toLocaleDateString("en-US", {
+					month: "long",
+				});
+				let unlockYear = date.toLocaleDateString("en-US", {
+					year: "numeric",
+				});
+				let displayDate = `${unlockDay} ${unlockMonth}, ${unlockYear}`;
+				return {
+					date,
+					asset: contractHolding.asset,
+					assetID: contractHolding.assetID,
+					price: null,
+					expiryDate: new Date(),
+					expiryTime: moment().utc().add(15, "minutes"),
+					unlockTime: contractHolding.unlockTime,
+					tokenDecimal: contractHolding.decimal,
+					quantity: contractHolding.actualBalance,
+					balance: contractHolding.actualBalance,
+					unlockDate: displayDate,
+					isContract: true,
+				};
+			})
+		);
+		userContractHoldings = userContractHoldings.filter(
+			(holding) => holding.quantity > 0
+		);
+	} catch (error) {
+		console.log(error);
+		// console.log("Holdings error");
+	}
+	return userContractHoldings;
 };
